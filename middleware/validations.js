ba785d84e3a5390d33fbde128b312e7f08f887cc
Facundo <facugastonbarral@gmail.com>
2022-07-24 22:57:20 -0300

    Fix token validation func

--- middleware/validations.js
+++ middleware/validations.js
@@ -1,4 +1,8 @@
-exports.validateEditToken = ( req, res ) => {
+exports.validateEditToken = ( req, res, next ) => {
     const { APP_EDIT_CODE } = process.env
-    const { editCode } = req.body 
+    const { editCode } = req.body
+    if (APP_EDIT_CODE !== editCode){
+        return res.status(401).send({success: false, message: 'Codigo de edición incorrecto. Acceso no permitido.'})
+    }
+    next()
 }

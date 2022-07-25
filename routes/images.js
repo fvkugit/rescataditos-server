7da44e0007bd80fd7e8a1cf22a8fc8cd71135801
Facundo <facugastonbarral@gmail.com>
2022-07-24 22:56:18 -0300

    Add endpoints for images

--- routes/images.js
+++ routes/images.js
@@ -1,8 +1,14 @@
 var express = require("express");
 var router = express.Router();
-const { uploadImage } = require("../controllers/imagesController");
-
+const { uploadImage, deleteImage, newImage } = require("../controllers/imagesController");
+const { validateEditToken } = require('../middleware/validations');
 
 router.post("/", express.json({limit: "2mb"}), uploadImage);
+
+router.use(express.json())
+router.use(express.urlencoded({extended: false}))
+
+router.delete("/:id", validateEditToken, deleteImage)
+router.post("/cargar", validateEditToken, newImage)
 
 module.exports = router;

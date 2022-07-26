0cb9318042e26f4ed63f672ecbba880646d6b8bf
Facundo <facugastonbarral@gmail.com>
2022-07-26 01:45:46 -0300

    Extend size limit on image post

--- routes/images.js
+++ routes/images.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const { uploadImage, deleteImage, newImage } = require("../controllers/imagesController");
 const { validateEditToken } = require('../middleware/validations');
 
-router.post("/", express.json({limit: "2mb"}), uploadImage);
+router.post("/", express.json({limit: "10mb"}), uploadImage);
 
 router.use(express.json())
 router.use(express.urlencoded({extended: false}))
